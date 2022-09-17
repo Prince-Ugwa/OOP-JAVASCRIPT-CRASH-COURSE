054b6c5a674eb88f13e86759dfffdae8ebaae0a7
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2022-09-17 13:11:14 +0100

    commit

--- starter/script.js
+++ starter/script.js
@@ -146,7 +146,7 @@ const car = function (make, speed) {
 
 car.prototype.accelerate = function () {
   this.speed += 10;
-  console.log(`${this.make} is going at ${this.speed} km/h`);
+  console.log(`${this.make} is running at ${this.speed} km/h`);
 };
 
 car.prototype.brake = function () {
@@ -204,73 +204,73 @@ mercedes.brake();
 // console.log(prince.age);
 // console.log(prince.__proto__ === personCl.prototype);
 
-class personCl {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
+// class personCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
 
-  //instance method
-  //we do not have to manually  write the protoype property in classes instead
-  //we write the ouside of the constructor function and then
-  // will atomaically be added to prototype of the class
-  calcAge() {
-    console.log(2023 - this.birthYear);
-  }
-  greet = function () {
-    console.log(`Helloo ${this.fullName}`);
-  };
-  get age() {
-    return 2037 - this.birthYear;
-  }
-  // set a property that already exist
-  set fullName(name) {
-    console.log(name);
-    //when we have a setter that is adding a property that already exist we include underscore to before the property name.this is just a convention for the variable name to avoid conflict
-    // as a convension to avoid naming conflict
-    if (name.includes(' ')) this._fullName = name;
-    else alert(`${name} is not a full name `);
-  }
-  get fullName() {
-    return this._fullName;
-  }
-}
+//   //instance method
+//   //we do not have to manually  write the protoype property in classes instead
+//   //we write the ouside of the constructor function and then
+//   // will atomaically be added to prototype of the class
+//   calcAge() {
+//     console.log(2023 - this.birthYear);
+//   }
+//   greet = function () {
+//     console.log(`Helloo ${this.fullName}`);
+//   };
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
+//   // set a property that already exist
+//   set fullName(name) {
+//     console.log(name);
+//     //when we have a setter that is adding a property that already exist we include underscore to before the property name.this is just a convention for the variable name to avoid conflict
+//     // as a convension to avoid naming conflict
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a full name `);
+//   }
+//   get fullName() {
+//     return this._fullName;
+//   }
+// }
 
-const prince = new personCl('Prince Odiniya', 1999);
-console.log(prince);
-// prince.calcAge();
-prince.greet();
-console.log(prince.age);
-console.log(prince.__proto__ === personCl.prototype);
-
-// note: we can use .prototype to add methods
-// personCl.prototype.greet = function () {
-//   console.log(`Helloo ${this.firstName}`);
-// };
+// const prince = new personCl('Prince Odiniya', 1999);
+// console.log(prince);
+// // prince.calcAge();
 // prince.greet();
+// console.log(prince.age);
+// console.log(prince.__proto__ === personCl.prototype);
 
-const jane = new personCl('Jane Mark', 2005);
+// // note: we can use .prototype to add methods
+// // personCl.prototype.greet = function () {
+// //   console.log(`Helloo ${this.firstName}`);
+// // };
+// // prince.greet();
 
-/////////////////////GETTERS AND SETTERS////////////////////////
-const account = {
-  owner: 'Prince',
-  movement: [200, 100, 50],
+// const jane = new personCl('Jane Mark', 2005);
 
-  get latest() {
-    return this.movement.slice(-1).pop();
-  },
+// /////////////////////GETTERS AND SETTERS////////////////////////
+// const account = {
+//   owner: 'Prince',
+//   movement: [200, 100, 50],
 
-  //any setter will need to have at least one parameter
-  set latest(mov) {
-    this.movement.push(mov);
-  },
-};
+//   get latest() {
+//     return this.movement.slice(-1).pop();
+//   },
 
-console.log(account.latest); // we do not call the latest as a method instead we write it as if it was a property
+//   //any setter will need to have at least one parameter
+//   set latest(mov) {
+//     this.movement.push(mov);
+//   },
+// };
 
-// note that the getters and setters are not method bt properties.
-account.latest = 250;
-console.log(account.movement);
+// console.log(account.latest); // we do not call the latest as a method instead we write it as if it was a property
+
+// // note that the getters and setters are not method bt properties.
+// account.latest = 250;
+// console.log(account.movement);
 
 /////////////////////////STATIC METHOD////////////////////
 
@@ -278,7 +278,7 @@ console.log(account.movement);
 //therefore all arrays do not inherit the method
 
 // .from, parseFloat etc
-console.log(Array.from(document.querySelectorAll('h1')));
+// console.log(Array.from(document.querySelectorAll('h1')));
 
 // we can not use static method on the array
 // const stat = [1, 2, 3].from();
@@ -295,30 +295,30 @@ console.log(Array.from(document.querySelectorAll('h1')));
 //there is no prototype involve and no constructor function involved and no new
 // we use the object.create to manually set the prototype of an object to any other object  we want.
 
-const personProto = {
-  calcAge() {
-    console.log(2027 - this.birthYear);
-  },
-  init(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  },
-};
+// const personProto = {
+//   calcAge() {
+//     console.log(2027 - this.birthYear);
+//   },
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
 
-const matthew = Object.create(personProto);
+// const matthew = Object.create(personProto);
 
-console.log(matthew);
+// console.log(matthew);
 
-matthew.name = 'matthew';
-matthew.birthYear = 2004;
-matthew.calcAge();
+// matthew.name = 'matthew';
+// matthew.birthYear = 2004;
+// matthew.calcAge();
 
-console.log(matthew.__proto__ === personProto);
+// console.log(matthew.__proto__ === personProto);
 
-const jude = Object.create(personProto);
-jude.init('jude', 1998);
-jude.calcAge();
-console.log(jude);
+// const jude = Object.create(personProto);
+// jude.init('jude', 1998);
+// jude.calcAge();
+// console.log(jude);
 
 //////////// key note: the object.create  creates a new object and the ///////
 /////////// prototype of that object is the object that we pass in //////////
@@ -365,14 +365,16 @@ ford.brake();
 ford.speedUs = 50;
 console.log(ford);
 */
-const person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-};
 
-person.prototype.calcAge = function () {
-  console.log(2027 - this.birthYear);
-};
+//////////////////INHERITANCE BETWEEN CONSTRUCTOR FUNCTIONS///////////////////
+// const person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// person.prototype.calcAge = function () {
+//   console.log(2022 - this.birthYear);
+// };
 
 // EXAMPLE 1: Do not do this..
 // const student = function (firstName, birthYear, course) {
@@ -382,17 +384,221 @@ person.prototype.calcAge = function () {
 // };
 
 /////////EXAMPLE 2: recommended
-const student = function (firstName, birthYear, course) {
-  //we use the call method to invoke the person object because,
-  //the call method allows a regular function call to enable 'this' on the function
-  // by defualt the this keyword point to undefined in a regular functio call
-  person.call(this, firstName, birthYear);
+// const student = function (firstName, birthYear, course) {
+//we use the call method to invoke the person object because,
+//the call method allows a regular function call to enable 'this' on the function
+// by defualt the this keyword point to undefined in a regular functio call
+
+//////////////////the method here make the student to inherit from the person object
+// person.call(this, firstName, birthYear);
+// this.course = course;
+// };
+
+//linking prototypes
+// student.prototype = Object.create(person.prototype); // this not correctly set to find the method
+
+// student.prototype.introduce = function () {
+//   console.log(`my name is ${this.firstName} and I study ${this.course}`);
+// };
+// const job = new student('Job', 2002, 'Accountancy');
+// job.introduce();
+// job.calcAge();
+
+// console.log(job instanceof student);
+// console.log(job instanceof person);
+// console.log(job instanceof Object);
+
+// student.prototype.constructor = student;
+
+// const person1 = function (fullName, birthYear) {
+//   this.fullName = fullName;
+
+//   this.birthYear = birthYear;
+// };
+// const prince2 = new person1('Prince Odiniya', 1999);
+// console.log(prince2);
+
+// const mark = new person1('mark obs', 2001);
+// console.log(mark);
+
+// person1.prototype.ageCalc = function () {
+//   2022 - this.birthyear;
+// };
+// const person1 = function (fullName, birthYear) {
+//   this.fullName = fullName;
+
+//   this.birthYear = birthYear;
+// };
+// person1.prototype.ageCalc = function () {
+//   console.log(2027 - this.birthYear);
+// };
+
+// const apprentice = function (fullName, birthYear, course) {
+//   person1.call(this, fullName, birthYear);
+//   this.course = course;
+// };
+// apprentice.prototype = Object.create(person1.prototype);
+
+// apprentice.prototype.greeting = function () {
+//   console.log('Hi there');
+// };
+// const mike = new apprentice('mike john', 2000, 'geography');
+// // console.log(mike);
+// mike.greeting();
+// mike.ageCalc();
+// apprentice.prototype.constructor = apprentice;
+
+// console.log(mike.__proto__);
+// console.log(mike.__proto__.__proto__);
+// console.log(mike instanceof apprentice);
+// console.log(mike instanceof person1);
+// console.dir(apprentice.prototype.constructor);
+
+/* 
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car.
+ Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge
+ to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20,
+ and decrease the charge by 1%. Then log a message like this: 'Tesla going at 
+ 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 
+'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! 
+HINT: Review the definiton of polymorphism 😉
+
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK:: 😀   
+const car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is running at ${this.speed} km/h`);
+};
+
+car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const Ev = function (make, speed, charge) {
+  car.call(this, make, speed);
+  this.charge = charge;
+};
+
+Ev.prototype = Object.create(car.prototype);
+Ev.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+Ev.prototype.accelerate = function () {
+  this.speed = this.speed + 20;
+  this.charge--;
+  console.log(`${this.make} is goin ${this.speed} and battery ${this.charge}`);
+};
+
+const tesla = new Ev('Tesla', 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.brake();
+tesla.accelerate();
+// tesla.brake();
+// tesla.accelerate();
+
+*/
+
+//////////////////INHERITANCE BETWEEN ES6 classes///////////////////
+class personCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  calcAge() {
+    console.log(2023 - this.birthYear);
+  }
+  greet = function () {
+    console.log(`Helloo ${this.fullName}`);
+  };
+  get age() {
+    return 2037 - this.birthYear;
+  }
+  set fullName(name) {
+    // console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name `);
+  }
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+// To implement inheritance between Es6 classes, we only need two ingredients.
+
+// We need.
+// 1.	The extends keyword: to make the child class inherit from the first class
+// 2.	The super function: is the constructor function of the parent class.
+//  Instead of calling the parent object with call method we use super
+//  function method to call the parent class. and pass the arg directly
+//the super function is responsible for creating the this keyword  in the sbclass
+// we must first have to define the super function before we can access the this keyword in the sub class
+
+//NOTE: IF YO DONT NEED ANY NEW METHOD THEN THERE IS NO NEED TO WRITE ANY CONSTRUCTOR FUNCTION IN THE CHILD CLASS
+
+class studentCl extends personCl {
+  constructor(fullName, birthYear, course) {
+    //always needs to happen first
+    super(fullName, birthYear);
+    this.course = course;
+  }
+  introdce() {
+    // console.log(`my name is ${this.fullName} and I study ${this.course}`);
+  }
+  calcAge() {
+    // console.log(
+    //   ` I'm a young man of ${
+    //     2027 - this.birthYear
+    //   } years of age but learning programming makes me feel alot younger like ${
+    //     2027 - this.birthYear - 5
+    //   }`
+    // );
+  }
+}
+const lawal = new studentCl('Lawal Jones', 1999, 'Medecine');
+lawal.introdce();
+lawal.calcAge();
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////INHERITANCE BETWEEN CLASSES: OBJECT.CREATE()///////////////////////////////
+
+const personProto = {
+  calcAge() {
+    console.log(2027 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+// const matthew = Object.create(personProto);
+
+// matthew.firstName = 'Matt';
+// matthew.birthYear = 2000;
+// matthew.calcAge();
+// console.log(matthew);
+
+const studentProto = Object.create(personProto);
+studentProto.init = function (firstName, birthYear, course) {
+  personProto.init.call(this, firstName, birthYear);
   this.course = course;
 };
 
-student.prototype.introduce = function () {
+studentProto.introdce = function () {
   console.log(`my name is ${this.firstName} and I study ${this.course}`);
 };
-const job = new student('Job', 2019, 'Accountancy');
-console.log(job);
-job.introduce();
+const jay = Object.create(studentProto);
+jay.init('Jay', 2000, 'Computer engineering');
+jay.introdce();
+jay.calcAge;
